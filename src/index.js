aa06a49b161534a98bb9319066170510ef2ab132
Amandee Ellawala <amamandee.manushika.e@gmail.com>
2025-02-19 10:52:53 +0000

    Initialized the project

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CreateAccount from './Pages/CreateAccount/CreateAccount';
+import Login from './Pages/Login/Login';
+import Home from './Pages/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<App/>}></Route>
+    <Route path='/create-account' element={<CreateAccount/>}></Route>
+    <Route path='/login' element={<Login/>}></Route>
+    <Route path='/home' element={<Home/>}></Route>
+  </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
