7b9de7a6ed666a2c463817cba7ca227e92999313
Amandee Ellawala <amamandee.manushika.e@gmail.com>
2025-02-19 11:46:21 +0000

    Created CreateAccount page and added create account functinalities using firebase APIs

---
+++ src/Pages/CreateAccount/CreateAccount.js
@@ -0,0 +1,71 @@
+import { Button, Checkbox, Flex, Form, Input, Layout } from "antd";
+import {  auth  } from '../../ firebase';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
+const CreateAccount = () => {
+    const navigate = useNavigate();
+
+    const onFinish = async (values) => {
+        console.log('Success:', values);
+        await createUserWithEmailAndPassword(auth, values.username, values.password).then((userCredential)=>{
+            const user = userCredential.user;
+            navigate("/login");
+            console.log(user);
+        }).catch((error)=>{
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+
+        })
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    return (
+        <div id="container">
+            <Flex justify='center'>
+                <h1>Sign Up</h1>
+            </Flex>
+            <Flex justify='center'>
+                <Form name="basic"
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 16 }}
+                    style={{ maxWidth: 1000 }}
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+
+                >
+                    <Form.Item
+                        label="Username"
+                        name="username"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+                    <Form.Item name="remember" valuePropName="checked" label={null}>
+                        <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
+                    <Form.Item label={null}>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Flex>
+        </div>
+    );
+}
+
+export default CreateAccount;
