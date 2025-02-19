9eba33fe5f67b92e1ea36fa6b74e0b54352e43d1
Amandee Ellawala <amamandee.manushika.e@gmail.com>
2025-02-19 11:58:25 +0000

    Firebase property file

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAUm-nXwD5VOhvjO8ULxZfPWk9L0uGkaSo",
+    authDomain: "workout-tracker-a251d.firebaseapp.com",
+    projectId: "workout-tracker-a251d",
+    storageBucket: "workout-tracker-a251d.firebasestorage.app",
+    messagingSenderId: "842687365367",
+    appId: "1:842687365367:web:7ef69c256f2a01ce78d3f2",
+    measurementId: "G-LLXVM6J8QP"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  export const auth = getAuth(app);
+  export default app;
