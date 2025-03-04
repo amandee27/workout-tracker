03cbe6d51322c4588c608ea1b98babc82b498231
Amandee Ellawala <amamandee.manushika.e@gmail.com>
2025-03-04 17:42:57 +0000

    Basic Calendar page

---
+++ src/Pages/Calendar/Calendar.js
@@ -0,0 +1,9 @@
+const Calendar = () => {
+  return (
+    <div className="container" style={{ height: '100vh' }}>
+      <h1>This is calendar page</h1>
+    </div>
+  );
+};
+
+export default Calendar;
