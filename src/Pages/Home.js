f1ec85e0f73ca73c53ce689fe2766175f2d18e35
Amandee Ellawala <amamandee.manushika.e@gmail.com>
2025-03-06 11:54:38 +0000

    Add header nav to home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,9 @@
 import { Layout } from 'antd';
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import SideNavBar from './SideNavBar';
+import HeaderNav from './HeaderNav';
+import { Content } from 'antd/es/layout/layout';
 
 const Home = () => {
   return (
@@ -12,7 +14,10 @@ const Home = () => {
     >
       <SideNavBar></SideNavBar>
       <Layout>
-        <Outlet></Outlet>
+        <HeaderNav></HeaderNav>
+        <Content style={{ backgroundColor: 'white', height: '100vh - 30px' }}>
+          <Outlet></Outlet>
+        </Content>
       </Layout>
     </Layout>
   );
