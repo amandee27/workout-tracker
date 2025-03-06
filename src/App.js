b0fa00c0d297167a25ef6b14912078aa2e04f441
Amandee Ellawala <amamandee.manushika.e@gmail.com>
2025-03-06 11:53:18 +0000

    Add new navigation routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
 import Login from './Pages/Login/Login';
 import CreateAccount from './Pages/CreateAccount/CreateAccount';
 import PrivateRoutes from './Shared/PrivateRoutes';
-import ForgotPassword from './Pages/ForgotPassword /ForgotPassword';
+import ForgotPassword from './Pages/ForgotPassword/ForgotPassword';
 import PublicRoutes from './Shared/PublicRoutes';
 import Calendar from './Pages/Calendar/Calendar';
 import UserDetails from './Pages/UserDetails/UserDetails';
 import Logs from './Pages/Logs/Logs';
-import Exercises from './Exercises/Exercises';
+import Exercises from './Pages/Exercises/Exercises';
+import LogExercise from './Pages/Exercises/LogExercise/LogExercise';
+import ExerciseDetails from './Pages/Exercises/ExerciseDetails/ExerciseDetails';
 
 function App() {
   return (
@@ -21,6 +23,9 @@ function App() {
             <Route path="user-detail" element={<UserDetails />} />
             <Route path="logs" element={<Logs />} />
             <Route path="exercises" element={<Exercises />} />
+            <Route path="log-exercise" element={<LogExercise />} />
+            <Route path="exercise-details" element={<ExerciseDetails />} />
+            <Route path="" element={<Navigate to={'exercises'} />} />
           </Route>
         </Route>
         <Route element={<PublicRoutes />}>
