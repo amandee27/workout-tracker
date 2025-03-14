02515867d650a3710b238e07c077b7fa2460f1dd
Amandee Ellawala <amamandee.manushika.e@gmail.com>
2025-03-14 21:00:54 +0000

    Fetch data from firebase for exercise detail page

--- src/Pages/Exercises/ExerciseDetails/ExerciseDetails.js
+++ src/Pages/Exercises/ExerciseDetails/ExerciseDetails.js
@@ -1,7 +1,43 @@
-import { Layout } from 'antd';
+import { Col, Layout, Row, Typography } from 'antd';
+import { useParams } from 'react-router-dom';
+import { doc, getDoc } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { db } from '../../../firebase';
 
 const ExerciseDetails = () => {
-  return <Layout style={{ minHeight: '100vh' }} align="center"></Layout>;
+  let { id } = useParams();
+  const docRef = doc(db, 'Exercises', id);
+  const [exercise, setExercise] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getDoc(docRef)
+      .then((doc) => {
+        setExercise(doc.data());
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, [loading]);
+
+  return (
+    <Layout style={{ minHeight: '100vh' }} align="center">
+      <div className="container" style={{ padding: 10 }}>
+        {!loading && (
+          <Row>
+            <Col span={12}>
+              <Typography.Title>{exercise.name.toUpperCase()}</Typography.Title>
+              <div style={{ backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh', width: 300 }}>
+                <img alt="Card image" src={exercise.image} />
+              </div>
+            </Col>
+            <Col span={12}></Col>
+          </Row>
+        )}
+      </div>
+    </Layout>
+  );
 };
 
 export default ExerciseDetails;
