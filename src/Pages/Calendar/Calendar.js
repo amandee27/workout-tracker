b6565c6a88967d5ce6aab1e99538dbad070320ed
Amandee Ellawala <amamandee.manushika.e@gmail.com>
2025-03-31 15:07:21 +0100

    Fixed issue not showing calender  when no data received from firebase and Added loading for calendar until data fetched

--- src/Pages/Calendar/Calendar.js
+++ src/Pages/Calendar/Calendar.js
@@ -1,4 +1,4 @@
-import { Calendar, Layout } from 'antd';
+import { Calendar, Layout, Skeleton } from 'antd';
 import { collection, getDocs } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 import { db } from '../../firebase';
@@ -6,8 +6,8 @@ import dayjs from 'dayjs';
 import 'dayjs/locale/zh-cn';
 
 const CalendarPage = () => {
-  const [loggedWorkouts, setLoggedWorkouts] = useState([]);
-  const [exerciseList, setExerciseList] = useState([]);
+  const [loggedWorkouts, setLoggedWorkouts] = useState(null);
+  const [exerciseList, setExerciseList] = useState(null);
   const [keyValueMap, setKeyValueMap] = useState({});
   const colRef = collection(db, 'Logs');
   const colRef2 = collection(db, 'Exercises');
@@ -46,28 +46,50 @@ const CalendarPage = () => {
 
   const getListData = (value) => {
     let listData = [];
-    loggedWorkouts.forEach((workout) => {
-      if (value.isSame(dayjs(workout?.date), 'day')) {
-        workout.workout.map((exercise) => {
-          if (keyValueMap[exercise.selectedExercise]) {
-            listData.push({ display: true, type: 'warning', content: keyValueMap[exercise.selectedExercise] });
-          }
-        });
-      }
-    });
+    if (
+      loggedWorkouts !== null &&
+      exerciseList !== null &&
+      loggedWorkouts.length > 0 &&
+      exerciseList.length > 0 &&
+      Object.keys(keyValueMap).length > 0
+    ) {
+      loggedWorkouts.forEach((workout) => {
+        if (value.isSame(dayjs(workout?.date), 'day')) {
+          workout.workout.map((exercise) => {
+            if (keyValueMap[exercise.selectedExercise]) {
+              listData.push({ display: true, type: 'warning', content: keyValueMap[exercise.selectedExercise] });
+            }
+          });
+        }
+      });
+    }
 
     return listData || [];
   };
 
   const dateCellRender = (value) => {
-    const listData = getListData(value);
+    let listData = [];
+    if (
+      loggedWorkouts !== null &&
+      exerciseList !== null &&
+      loggedWorkouts.length !== 0 &&
+      exerciseList.length !== null &&
+      Object.keys(keyValueMap).length > 0
+    ) {
+      listData = getListData(value);
+    }
 
-    return (
-      <ul className="events">
-        {!(listData.length === 1 && listData[0].length === 0) &&
-          listData.map((item) => <li key={item.content}>{item.content && <p>{item.content}</p>}</li>)}
-      </ul>
-    );
+    if (loggedWorkouts === null && exerciseList === null) {
+      return <Skeleton.Input active={true} size="small" />;
+    } else
+      return (
+        <ul className="events" style={{ listStyleType: 'none', color: 'red' }}>
+          {!(listData.length === 1 && listData[0].length === 0) &&
+            listData.map((item) => (
+              <li key={item.content}>{item.content && <p style={{ fontSize: 12 }}>{item.content}</p>}</li>
+            ))}
+        </ul>
+      );
   };
   const cellRender = (current, info) => {
     if (info.type === 'date') return dateCellRender(current);
@@ -76,9 +98,7 @@ const CalendarPage = () => {
   return (
     <Layout style={{ minHeightt: '100vh' }} align="center">
       <div style={{ padding: 30 }}>
-        {loggedWorkouts.length !== 0 && exerciseList.length !== 0 && Object.keys(keyValueMap).length > 0 && (
-          <Calendar cellRender={cellRender} />
-        )}
+        <Calendar cellRender={cellRender} />
       </div>
     </Layout>
   );
